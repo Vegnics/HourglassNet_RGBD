4f88f25e14f421be0df5c61177421f2a68b45056
Paulo Linares <pe_linareso@hotmail.com>
2024-06-02 09:38:51 +0800

    crying

--- hourglass_tensorflow/losses/mae_custom.py
+++ hourglass_tensorflow/losses/mae_custom.py
@@ -18,7 +18,7 @@ class MAE_custom(keras.losses.Loss):
         #W = tf.constant([3.0,2.0,3.0],dtype=tf.dtypes.float32)
         #W = tf.constant([0.48,0.52],dtype=tf.dtypes.float32)
         #W = tf.constant([0.32,0.33,0.35],dtype=tf.dtypes.float32)
-        W = tf.constant([0.33,0.34,0.36],dtype=tf.dtypes.float32)
+        W = tf.constant([0.31,0.32,0.33],dtype=tf.dtypes.float32)
         W = tf.reshape(W,[1,-1,1,1,1])
         #Rmax = tf.sqrt(tf.constant(2.0,dtype=tf.float32))*tf.constant(64.0,dtype=tf.float32)
         #cy_true = tf.exp(-0.5*tf.square(Rmax*tf.cast(y_true,dtype=tf.dtypes.float32)/255.0))
@@ -36,10 +36,12 @@ class MAE_custom(keras.losses.Loss):
         dist= tf.reduce_sum(dist,axis=[1,2])/64.0 #NC
         dist = tf.reduce_sum(dist,axis=1)# N
         """
+        dist1 = tf.reduce_sum(dist1,axis=4)
+        dist1 = tf.reduce_mean(dist1,axis=[2,3])
         dist1 = tf.reduce_sum(dist1,axis=1) #NHWC
         #dist1 = tf.reduce_sum(dist1,axis=4) #NSHW
-        dist1 = tf.reduce_mean(dist1,axis=3)
-        dist1 = tf.reduce_mean(dist1,axis=[1,2]) #NS
+        #dist1 = tf.reduce_mean(dist1,axis=3)
+        #dist1 = tf.reduce_mean(dist1,axis=[1,2]) #NS
         #dist1 = tf.reduce_mean(dist1,axis=[1,2]) 
         #dist1= tf.sqrt(tf.reduce_mean(dist1,axis=[1,2])) #N
         #dist1 = tf.reduce_sum(dist1,axis=1) #N

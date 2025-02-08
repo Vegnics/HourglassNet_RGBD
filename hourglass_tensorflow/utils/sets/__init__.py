c2405d41c947cef32b685e48ecb0179496d6a661
Quinoa <pe.lotoya93@gmail.com>
2025-02-08 14:16:17 +0800

    testonlinetf218

--- hourglass_tensorflow/utils/sets/__init__.py
+++ hourglass_tensorflow/utils/sets/__init__.py
@@ -8,6 +8,6 @@ def split_train_test(items: Set[str], ratio: float = 0.8) -> Tuple[Set[str], Set
 
     length = len(items)
     items_to_select = int(length * ratio)
-    train_samples = set(random.sample(items, items_to_select))
+    train_samples = set(random.sample(list(items), items_to_select))
     test_samples = set([item for item in items if item not in train_samples])
     return train_samples, test_samples

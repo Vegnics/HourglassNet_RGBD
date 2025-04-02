b221c1f98a7d05d1cf4f0268739d009940caa3c1
Quinoa <pe.lotoya93@gmail.com>
2025-04-02 14:38:37 +0800

    Model config modified 04_02

--- hourglass_tensorflow/handlers/model.py
+++ hourglass_tensorflow/handlers/model.py
@@ -180,6 +180,32 @@ class HTFModelHandler(_HTFModelHandler):
                             layer.merge_feats_main.trainable = False
                             print(f"Freezing {main_name}/{layer.merge_feats_1j.name}")
                             layer.merge_feats_1j.trainable = False
+                elif self.config.loading_style == "Partial_Train_Attention":
+                    print(">>>>>>>>>> [LOADING] PARTIAL TRAINING FOR ATTENTION <<<<<<<<<<<<<<")
+                    for layer in model.layers:
+                        if isinstance(layer,DownSamplingLayer):
+                            print(f"Freezing {layer.name}")
+                            layer.trainable = False
+                        elif isinstance(layer,HourglassLayer):
+                            main_name = layer.name
+                            # Train only the main hourglass
+                            for _,val in layer.layer_list.items():
+                                for _,v in val.items():
+                                    if isinstance(v,Layer):
+                                        #print(f"Freezing {main_name}/{v.name}")
+                                        v.trainable = True
+                            print(f"Freezing {main_name}/{layer.residual_brc.name}")
+                            layer.residual_brc.trainable = False
+                            print(f"Freezing {main_name}/{layer.merge_feats_main.name}")
+                            layer.merge_feats_main.trainable = False
+                            print(f"Freezing {main_name}/{layer.merge_feats_1j.name}")
+                            layer.merge_feats_1j.trainable = False
+                            print(f"Freezing {main_name}/{layer.hm2_output.name}")
+                            layer.hm2_output.trainable = False
+                            print(f"Freezing {main_name}/{layer.features_hm2.name}")
+                            layer.features_hm2.trainable = False
+                            print(f"Freezing {main_name}/{layer.residual_2j.name}")
+                            layer.residual_2j.trainable = False
                 elif self.config.loading_style == "Full_Train":
                     print(">>>>>>>>>> [LOADING] FULL TRAINING <<<<<<<<<<<<<<")               
                         #for _l in HourglassLayer.layers:

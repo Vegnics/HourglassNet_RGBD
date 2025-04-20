a5933b94a84c59eda5502140e4c28652a27e9e65
Quinoa <pe.lotoya93@gmail.com>
2025-04-20 12:37:46 +0800

    new version of AMs

--- hourglass_tensorflow/layers/attention_spatial.py
+++ hourglass_tensorflow/layers/attention_spatial.py
@@ -119,12 +119,12 @@ class SpatialEnergyHead(Layer):
                 name = "Vgen_FC",
                 )
         
-        self.diff_vec = self.add_weight(
-                shape=[1,16],
-                name="diff_vec",
-                initializer="glorot_normal",
-                trainable=self.trainable,
-            )
+        #self.diff_vec = self.add_weight(
+        #        shape=[1,16],
+        #        name="diff_vec",
+        #        initializer="glorot_normal",
+        #        trainable=self.trainable,
+        #    )
 
         self.Hgen = layers.Dense(self.K_size,
                 activation=None, #None,
@@ -146,16 +146,15 @@ class SpatialEnergyHead(Layer):
         }
 
     def call(self, inputs: tf.Tensor, training: bool = True) -> tf.Tensor: # training = True
-        input_v = tf.transpose(tf.stack((inputs,inputs-self.diff_vec),axis=-1),perm=[0,2,1])
-        input_h = tf.transpose(tf.stack((inputs,inputs+self.diff_vec),axis=-1),perm=[0,2,1])
-
-        v = self.dropout_v(self.Vgen(input_v), training=training)  # (B,K,2)
-        h = self.dropout_h(self.Hgen(input_h), training=training)  # (B,K,2)
-
-        V = tf.nn.softmax(v, axis=-1)                     # (B,2,K)
-        H = tf.nn.softmax(h, axis=-1)                     # (B,2,K)
-
-        V = tf.transpose(V,perm=[0,2,1])                # (B,K,2)
+        #input_v = tf.transpose(tf.stack((inputs,inputs-self.diff_vec),axis=-1),perm=[0,2,1])
+        #input_h = tf.transpose(tf.stack((inputs,inputs+self.diff_vec),axis=-1),perm=[0,2,1])
+        _input = tf.transpose(inputs,perm=[0,2,1]) #(B,3,K)
+        #input_h = tf.transpose(tf.stack((inputs,inputs+self.diff_vec),axis=-1),perm=[0,2,1])
+        v = tf.nn.relu(self.Vgen(_input))
+        h = tf.nn.relu(self.Hgen(_input ))   
+        V = self.dropout_v(tf.math.l2_normalize(v), training=training)  # (B,3,K)
+        H = self.dropout_h(tf.math.l2_normalize(h), training=training)  # (B,3,K)
+        V = tf.transpose(V,perm=[0,2,1])                # (B,K,3)
         #H = tf.reshape(H, (-1, 2, self.K_size))                # (B,2,K)
 
         return tf.matmul(V, H)                            # (B,K,K)
@@ -214,6 +213,7 @@ class SpatialAttentionMechanism(Layer):
             use_bias=False,
         )
         self.bn = layers.BatchNormalization(axis=-1,
+                                            momentum=0.9,
                                             trainable=self.trainable,
                                             name="BN_sam")
         
@@ -234,8 +234,8 @@ class SpatialAttentionMechanism(Layer):
             name="AttConv2D_scores",
             activation=None,
             kernel_regularizer= RegL2(1e-6) if self.kernel_reg else None,
-            kernel_initializer= "zeros",
-            bias_initializer=tf.constant_initializer(-3.0),
+            kernel_initializer= "glorot_uniform",
+            #bias_initializer=tf.constant_initializer(-3.0),
             use_bias=True,
         )
         
@@ -265,9 +265,14 @@ class SpatialAttentionMechanism(Layer):
         #_inputs = tf.reduce_mean(tf.math.square(inputs),keepdims=True,axis=-1)
         tiled = tf.reshape(projection, (-1, 4, K // 4, 4, K // 4))
         tiled = tf.transpose(tiled, perm=[0, 1, 3, 2, 4])
-        energy_descriptor = self.ln(tf.reshape(tf.reduce_mean(tf.math.square(tiled),axis=[3,4]),(-1,16)))
+        tgrid = tf.range(0,4,1,dtype=tf.float32)
+        X,Y = tf.meshgrid(tgrid,tgrid)
+        energy_tile = tf.reduce_mean(tf.math.square(tiled),axis=[3,4])
+        pos_encoding = tf.repeat(tf.stack([X,Y],axis=-1),repeats=tf.shape(inputs)[0],axis=0)
+        energy_descriptor = tf.reshape(tf.concat([energy_tile,pos_encoding],axis=-1),(-1,16,3))
         stacked_outs = tf.stack([self.spatial_heads[u](energy_descriptor,training=training) for u in range(self.head_num)],axis=-1)
-        scores = tf.nn.sigmoid(self.score_gen(stacked_outs)) 
+        stacked_outs = self.ln(stacked_outs)
+        scores = tf.clip_by_value(tf.nn.swish(self.score_gen(stacked_outs)),0.0,1.0) 
         return scores
     
     def build(self, input_shape):

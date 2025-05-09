5bae514a18a4f46d5b1dac2db68e93d81bb369bd
Quinoa <pe.lotoya93@gmail.com>
2025-05-09 09:36:59 +0800

    fixing AMs

--- scripts/config_generation/train_config_generator.py
+++ scripts/config_generation/train_config_generator.py
@@ -30,7 +30,7 @@ limbs_2J = [(0,1),(1,2),(2,3),(3,4),(4,5),(6,7),(7,8),(8,12),(9,12),(12,13),(11,
 enable_visibility = False
 
 # Related to the model and training
-model_name = "myModel_SLP_WS_BL_1B_ATT9FTTEST_3_Depth4C.keras" #"myModel_SLP_WS_BL_1B_w2joints.keras"
+model_name = "myModel_SLP_WS_BL_WithAtt2_Depth4C.keras" #"myModel_SLP_WS_BL_1B_w2joints.keras"
 chkpnt_path = os.path.join("data/model_t",model_name)
 csv_logger = f"logs/myModelLogs_{model_name}_{current_time}.csv"
 epochs = 150
@@ -38,17 +38,17 @@ batch_size = 20
 stages = 2
 stage_filters = 256
 residual_nblocks = 1
-learning_rate = 1.5e-6
+learning_rate = 1.5e-5
 
 # Related to loading pre-trained models
 load_model = True
-pt_model_name = "data/model_t/myModel_SLP_WS_BL_1B_FT2_Depth4C.keras" #"data/model_t/myModel_SLP_WS_BL_1B_ATT9FTTEST_2_Depth4C.keras" #"/home/quinoa/Documents/models_slp/myModel_SLP_BL_1B_HG1Depth4C.keras" #"data/model_t/myModel_SLP_WS_BL_1B_FT2_Depth4C.keras" # # #"data/model_t/myModel_SLP_WS_BL_1B_ATT9FT_Depth4C.keras" #
-loading_mode =  "Partial_Train_Attention" #"Partial_Train_F2S" # #"Partial_Downsampling_frozen" # # # # # #"Partial_Downsampling_frozen" # #"Full_Train" #"Partial_Downsampling_frozen"
+pt_model_name = "data/model_t/myModel_SLP_WS_BL_WithAtt_Depth4C_FT1.keras" #"data/model_t/myModel_SLP_WS_BL_1B_FT2_Depth4C.keras" #"data/model_t/myModel_SLP_WS_BL_1B_ATT9FTTEST_2_Depth4C.keras" #"/home/quinoa/Documents/models_slp/myModel_SLP_BL_1B_HG1Depth4C.keras" #"data/model_t/myModel_SLP_WS_BL_1B_FT2_Depth4C.keras" # # #"data/model_t/myModel_SLP_WS_BL_1B_ATT9FT_Depth4C.keras" #
+loading_mode =  "Partial_Train_F2S"#"Full_Train" #"Partial_Train_Attention" #"Partial_Train_S2F" # # # #"Partial_Downsampling_frozen" # # # # # #"Partial_Downsampling_frozen" # #"Full_Train" #"Partial_Downsampling_frozen"
 
 #Related to the attention mechanisms
 skip_AM = "NoAM"
 s2f_AM =  "FAM" 
-f2s_AM = "SAM" 
+f2s_AM = "FAM" 
 
 #Related to the loss
 W_1jnts = 1.0

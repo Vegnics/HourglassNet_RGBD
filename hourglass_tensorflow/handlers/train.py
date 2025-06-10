d901c12a85b8af79f47c29b2cbe98e3f4bbb881b
Amaranth <d12922028@ntu.edu.tw>
2025-06-10 17:39:20 +0800

    experimental modifications to HG and ResidualBlock

--- hourglass_tensorflow/handlers/train.py
+++ hourglass_tensorflow/handlers/train.py
@@ -125,7 +125,7 @@ class HTFTrainHandler(_HTFTrainHandler):
             train_dataset = train_dataset.shuffle(tds_card,reshuffle_each_iteration=True)
             #validation_dataset = validation_dataset.shuffle(vds_card,reshuffle_each_iteration=True)
             
-            train_dataset = train_dataset.repeat(2) #7 for SLP
+            #train_dataset = train_dataset.repeat(2) #7 for SLP
             #train_dataset = train_dataset.repeat(12) #12 is for DCCV
             
             #batch_testimgs = imgs_ds.batch(80) 

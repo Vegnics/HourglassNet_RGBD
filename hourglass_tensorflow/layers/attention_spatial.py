42a85cf4eef4dac49b573a31c62ba10e714ecc73
Amaranth <d12922028@ntu.edu.tw>
2025-06-13 15:46:41 +0800

    modifying var regularization

--- hourglass_tensorflow/layers/attention_spatial.py
+++ hourglass_tensorflow/layers/attention_spatial.py
@@ -300,7 +300,7 @@ class SpatialAttentionMechanism(Layer):
         # Enforce higher variance at the scores
         scores_mean = tf.reduce_mean(scores_raw,axis=[1,2],keepdims=True)
         scores_var = tf.reduce_mean(tf.square(scores_raw-scores_mean),axis=[1,2])
-        var_reg = tf.reduce_mean(1/tf.maximum(scores_var,1e-4))
+        var_reg = tf.reduce_mean(1/tf.maximum(scores_var,0.01))
         self.add_loss(1e-6*var_reg)
         return scores_raw # (B,K,K,1)=(B,H,W,1)
     

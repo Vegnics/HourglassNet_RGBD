e897e93fc9492777e4d4a891f2fcb718a46d6cee
Amaranth <d12922028@ntu.edu.tw>
2025-07-02 18:53:34 +0800

    experimental SAM

--- hourglass_tensorflow/layers/residual.py
+++ hourglass_tensorflow/layers/residual.py
@@ -10,7 +10,7 @@ from hourglass_tensorflow.layers.conv_block import ConvBlockLayer
 from hourglass_tensorflow.layers.conv_batch_norm_relu import ConvBatchNormReluLayer
 from hourglass_tensorflow.layers.dummy_layers import zeroLayer,IdentityLayer, quasiConstantLayer
 from hourglass_tensorflow.layers.attention_feature import FeatureAttentionMechanism
-from hourglass_tensorflow.layers.attention_spatial import SpatialAttentionMechanism
+from hourglass_tensorflow.layers.attention_spatial3 import SpatialAttentionMechanism
 
 
 @register_keras_serializable(package="lResiduals")
